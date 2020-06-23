88071e325396de203f26699bda75c611d1d337ed
Tamanji Che <cheambetamanji@gmail.com>
2020-06-24 01:22:45 +0300

    models/blog module

--- index.js
+++ index.js
@@ -6,36 +6,7 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
-
-
-
-mongoose.set('useFindAndModify', false)
-
-const blogSchema = mongoose.Schema({
-  title: {
-      type: String,
-      required: true,
-    },
-  author:{
-       type: String,
-       required: true,
-    },
-  url: {
-      type: String,
-      required: true,
-    },
-  likes: Number
-})
-
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
