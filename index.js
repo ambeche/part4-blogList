6010efe0b4d2b464e4adc7ec5c6969382ce34e68
Tamanji Che <cheambetamanji@gmail.com>
2020-06-24 01:42:01 +0300

    blogs controllers extracted

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -14,24 +15,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.morganLogger(':method :url :status :res[content-length] - :response-time ms :data'))
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs.map( blog => blog.toJSON()))
-    })
-})
-
-app.post('/api/blogs', (request, response, next) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result.toJSON())
-    })
-    .catch(error => next(error))
-})
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownRoute)
 app.use(middleware.errorHandler)
